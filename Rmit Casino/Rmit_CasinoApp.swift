94a20089acbeb57e93ce1d9f3594c13f49ba26ce
DaiHoang <hoangquocdai1411@gmail.com>
2022-08-27 16:15:06 +0700

    Initial Commit

---
+++ Rmit Casino/Rmit_CasinoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rmit_CasinoApp.swift
+//  Rmit Casino
+//
+//  Created by Tri Nguyen on 27/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct Rmit_CasinoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
